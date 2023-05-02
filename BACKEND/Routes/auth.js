ee3bd65a4dc2be389a4846a92ed7ce8abf23bb2d
Oscar Anillo <oscarani14@gmail.com>
2023-05-02 15:26:42 -0500

    add routes for register/login

--- BACKEND/Routes/auth.js
+++ BACKEND/Routes/auth.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
-const User = require("../Model/User")
+const User = require("../Model/User");
+const bcrypt = require("bcrypt")
 
 // Sign up
 router.post("/register", async (req, res) => {
     try {
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt)
+
         const body = req.body;
         const newUser = new User({
             fullName: body.fullName,
             email: body.email,
-            password: body.password
+            password: hashPassword
         });
         const user = await newUser.save()
         res.status(200).json(user)
@@ -22,6 +27,10 @@ router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email })
         !user && res.status(404).json("User not found!")
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("Incorrect Password!")
+
         res.status(200).json(user)
     } catch (err) {
         res.status(500).json(err)
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors")
 
 dotenv.config();
 const authRoute = require("./Routes/auth.js")
 
 const PORT = 3005;
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/auth", authRoute)
 
